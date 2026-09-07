51e81aaa9867c8978300b0ffa14a3d82552e8831
phadeline <phadelinee@gmail.com>
2026-09-07 19:06:29 -0400

    requests complete

---
+++ space.py
@@ -0,0 +1,17 @@
+import requests
+
+r = requests.get('http://api.open-notify.org/astros.json')
+
+print("find out how many people are in space at the moment:")
+
+json = r.json()
+
+
+print("there are a total of:", json['number'], "people in space.")
+
+for peeps in json['people']:
+    print(peeps['name'], "is on the", peeps['craft'])
+
+
+    
+
